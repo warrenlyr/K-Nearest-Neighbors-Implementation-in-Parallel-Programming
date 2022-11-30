b611eced43491f1b1fe33f257c36c9c91765a156
Warren <29391926+warrenlyr@users.noreply.github.com>
2022-11-30 02:39:27 -0800

    Updated test data generator v2, Completed test data generator v2 data visualization

--- TestFileGenerator/generator_2_have_class/main.py
+++ TestFileGenerator/generator_2_have_class/main.py
@@ -1,9 +1,21 @@
 '''
+---------------------------------------------------------------------------
+Description:
+Clean multiple datasets and merge into one
+data file that we can use for our final project.
+Use Python because it's the easiest and fastest way
+to develop the tool.
+---------------------------------------------------------------------------
+Datasets source:
+https://www.kaggle.com/datasets/selfishgene/historical-hourly-weather-data
+---------------------------------------------------------------------------
 Author: Warren Liu
 Date: 11/30/2022
+---------------------------------------------------------------------------
 '''
 
 import os
+import matplotlib.pyplot as plt
 
 ORG_DATA_PATH = os.path.join(
     os.path.dirname(os.path.abspath(__file__)),
@@ -60,9 +72,56 @@ def merge_datasets() -> bool:
             pp = pp.strip()
             tt = tt.strip()
             ww = ww.strip()
+            
+            # do some filter on the weather
+            if 'rain' in ww.lower():
+                ww = 'rain'
+            elif 'snow' in ww.lower():
+                ww = 'snow'
+            elif 'clear' in ww.lower():
+                ww = 'clear'
+            elif 'clouds' in ww.lower():
+                ww = 'clouds'
+            else:
+                continue
+
             if len(hh) and len(pp) and len(tt) and len(ww):
                 merged_data.write(f'{hh},{pp},{tt},{ww}\n')
+    
+    merged_data.close()
+    return True
+
+
+def show_data(input_file: str) -> None:
+    with open(input_file, 'r', 1, 'utf8') as f:
+        fig = plt.figure()
+        ax = fig.add_subplot(projection='3d')
+        ax.set_xlabel('Humidity')
+        ax.set_ylabel('Pressure')
+        ax.set_zlabel('Temperature')
+
+        cnt = 0
+        for line in f.readlines():
+            if cnt > 10000: break
+            x, y, z, weather = line.split(',')
+            weather = weather.strip()
+
+            c = 'black'
+            if weather == 'rain':
+                c = 'blue'
+            elif weather == 'snow':
+                c = 'black'
+            elif weather == 'clear':
+                c = 'green'
+            else:
+                c = 'gray'
+
+            ax.scatter(float(x), float(y), float(z), c=c)
+            cnt += 1
+
+        plt.show()
 
 
 if __name__ == '__main__':
-    merge_datasets()
+    # merge_datasets()
+    show_data(os.path.join(MERGED_DATA_PATH, 'data.csv'))
