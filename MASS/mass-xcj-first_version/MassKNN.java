e39a4dd12b11811a49dcce1ffb4fbd620589ea4a
ChengjunXi <xcjtom@gmail.com>
2022-12-09 15:07:24 -0800

    flat place version

---
+++ MASS/mass-xcj-first_version/MassKNN.java
@@ -0,0 +1,144 @@
+package edu.uwb.xi7;
+
+import java.util.Date;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.io.Serializable;
+
+import edu.uw.bothell.css.dsl.MASS.MASS;
+import edu.uw.bothell.css.dsl.MASS.Places;
+import edu.uw.bothell.css.dsl.MASS.logging.LogLevel;
+import java.io.FileNotFoundException;
+
+public class MassKNN {
+	@SuppressWarnings("unused")		// some unused variables left behind for easy debugging
+	public static void main(String[] args) throws FileNotFoundException{
+        int k = Integer.parseInt(args[0]);
+        // Points are stored as <"X Y","Class"> for groundTruth and result
+        HashMap<String, String> groundTruth = new HashMap<>();
+        HashMap<String, String> result = new HashMap<>();
+        // targets contains the points to be classified
+		ArrayList<Point> targets = new ArrayList<>();
+		final String classNames[] = { "clear", "clouds", "rain" };
+
+		ArrayList<Point> train_arr = new ArrayList<>();
+		Scanner input = new Scanner(new File("train.csv"));
+		while (input.hasNextLine()) {
+			String line = input.nextLine();
+			String[] words = line.split(",");
+			int classNum;
+			if(words[3].equals("clear")){
+				classNum=0;
+			}
+			else if(words[3].equals("clouds")){
+				classNum=1;
+			}
+			else{
+				classNum=2;
+			}
+			train_arr.add(new Point(Double.parseDouble(words[0]), Double.parseDouble(words[1]),
+					Double.parseDouble(words[2]), classNum));
+		}
+
+		Object train[] = train_arr.toArray();
+
+		// Read test points
+		input = new Scanner(new File("test.csv"));
+		int numTest = input.nextInt();
+		input.nextLine();
+		for (int i = 0; i < numTest; i++) {
+			String line = input.nextLine();
+			String[] words = line.split(",");
+			targets.add(new Point(Double.parseDouble(words[0]), Double.parseDouble(words[1]),
+					Double.parseDouble(words[2]), -1));
+			groundTruth.put(words[0] + " " + words[1] + " " + words[2], words[3]);
+		}
+
+		// remember starting time
+		long startTime = new Date().getTime();
+		
+		// init MASS library
+		MASS.setNodeFilePath("nodes.xml");
+		MASS.setLoggingLevel(LogLevel.ERROR);
+		// start MASS
+		MASS.init();
+		
+		// Create all places
+		Places places = new Places(1, PointPlace.class.getName(), null, train_arr.size());
+		places.callAll(PointPlace.init, train);
+		
+		for (Point target : targets) {
+			// Calculate distance
+			places.callAll(PointPlace.dist, target);
+
+			// (Object[])null is not a good interface design
+			Object allDistances[]=places.callAll(PointPlace.collect, (Object[])null);
+			
+			ArrayList<ClassDist> distances = new ArrayList<>();
+			for (int i = 0; i < train.length; i++) {
+				distances.add(new ClassDist(train_arr.get(i).classNum, (Double)allDistances[i]));
+			}
+
+			distances.sort(new DistComp());
+
+			distances = new ArrayList<ClassDist>(distances.subList(0, k));
+
+            // Do majority voting
+            // Find max class number
+            int maxClass = 0;
+            for (int i = 0; i < k; i++) {
+                if (distances.get(i).classNum > maxClass) {
+                    maxClass = distances.get(i).classNum;
+                }
+            }
+            // Vote
+            int votes[] = new int[maxClass + 1];
+            for (int i = 0; i < k; i++) {
+                votes[distances.get(i).classNum] += 1;
+            }
+            // Find the winning class
+            int winClass = 0;
+            int maxVote = 0;
+            for (int i = 0; i < maxClass + 1; i++) {
+                if (votes[i] > maxVote) {
+                    maxVote = votes[i];
+                    winClass = i;
+                }
+            }
+
+            result.put(target.x + " " + target.y + " " + target.z, classNames[winClass]);
+		}
+
+		MASS.finish();
+		MASS.getLogger().debug( "MASS has stopped" );
+		
+        // Evaluate acc (accuracy)
+        double acc=0;
+        for (Entry<String,String> e : result.entrySet()) {
+            if (groundTruth.get(e.getKey()).equals(e.getValue())) {
+                acc++;
+            }
+        }
+        acc /= numTest;
+        System.out.println("acc: " + acc);
+
+		// calculate / display execution time
+		long execTime = new Date().getTime() - startTime;
+		System.out.println( "Execution time = " + execTime + " ms" );
+		
+	 }
+	 
+}
+
+// Comparator class for ClassDist
+class DistComp implements Comparator<ClassDist>, Serializable {
+    public int compare(ClassDist v1, ClassDist v2) {
+        return Double.compare(v1.dist,v2.dist);
+    }
+}
